472472362ad848fa9258f3b4c0fb9adbbb2b85b3
LiviuSosu <sosuliviu@gmail.com>
2022-08-26 07:38:34 +0200

    ?

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
-console.log('Hello, world!');
 //https://auth0.com/blog/node-js-and-express-tutorial-building-and-securing-restful-apis/
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+// defining the Express app
+const app = express();
+
+// defining an array to work as the database (temporary solution)
+const ads = [
+  {title: 'Hello, world (again)!'}
+];
+
+// adding Helmet to enhance your API's security
+app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+
+// defining an endpoint to return all ads
+app.get('/', (req, res) => {
+  res.send(ads);
+});
+
+// starting the server
+app.listen(3001, () => {
+  console.log('listening on port 3001');
+});
